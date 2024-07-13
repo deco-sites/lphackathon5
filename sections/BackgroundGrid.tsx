49121008d04e4ae33cf345c5f14a3dfd8cc20b9a
Marco Ferreira <mw.marcowilliam@gmail.com>
2024-07-12 23:47:28 -0300

    feat: padrão de background ao fundo dos componentes

---
+++ sections/BackgroundGrid.tsx
@@ -0,0 +1,14 @@
+export default function BackgroundGrid(){
+  return (
+    <div class="fixed w-full h-full px-6 lg:px-20">
+      <div class="w-full h-full border-base-content" style={{
+        borderLeft: "3px solid",
+        background: "white",
+        backgroundImage: "radial-gradient(black 1px, transparent 0)",
+        backgroundSize: "19px 19px",
+        backgroundPosition: "-19px -19px",
+      }}>
+      </div>
+    </div>
+  )
+}
